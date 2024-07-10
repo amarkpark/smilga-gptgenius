1921db3956832070f58f387ea6b12780b3fdcc53
Amanda Mark <amarkpark@gmail.com>
2024-07-10 14:40:09 -0600

    Lesson 63: Build main menu in dashboard layout

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -1,7 +1,32 @@
+import Sidebar from "@/components/Sidebar"
+// import { IconName } from "react-icons/fi";
+import { FiMenu } from "react-icons/fi";
+
 function DashLayout({ children }) {
   return (
-    <div>
-      {children}
+    <div className="drawer lg:drawer-open">
+      <input id="main-menu-drawer" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content">
+        <label
+          htmlFor="main-menu-drawer"
+          className="drawer-button lg:hidden fixed top-6 right-6"
+          aria-label="open-sidebar"
+        >
+          <FiMenu className="w-8 h-8 text-primary" />
+        </label>
+        <div className="bg-base-200 px-8 py-12 min-h-screen">
+          {children}
+        </div>
+      </div>
+      <div className="drawer-side">
+        <label
+          htmlFor="main-menu-drawer"
+          className="drawer-overlay"
+          aria-label="close-sidebar"
+        >
+        </label>
+        <Sidebar />
+      </div>
     </div>
   )
 }
---
+++ components/Sidebar.jsx
@@ -0,0 +1,23 @@
+// this is `rce`
+// export class Sidebar extends Component {
+//   render() {
+//     return (
+//       <div className="text-6xl">
+//         Sidebar
+//       </div>
+//     )
+//   }
+// }
+
+// export default Sidebar
+
+// this is `rafce`
+const Sidebar = () => {
+  return (
+    <div className="text-6xl">
+        Sidebar
+    </div>
+  )
+}
+
+export default Sidebar
