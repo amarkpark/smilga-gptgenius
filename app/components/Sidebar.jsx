0b418d6583555557dc5254463c5d8328b54bb772
Amanda Mark <amarkpark@gmail.com>
2024-07-10 17:07:59 -0600

    Horrendously wonky shite is happening between VSCode and Git ignoring modifications

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -4,6 +4,8 @@ import NavLinks from "./NavLinks"
 import MemberProfile from "./MemberProfile"
 
 const Sidebar = () => {
+  console.log("Sidebar Eh?");
+  
   return (
     <div
       className=" px-4 w-80 min-h-full bg-base-300 py-12 grid grid-rows-[auto,1fr,auto]"
