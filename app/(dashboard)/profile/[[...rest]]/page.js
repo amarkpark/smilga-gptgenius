b2f4991ccecd303eb06092dedcd78108b63bd173
Amanda Mark <amarkpark@gmail.com>
2024-07-11 17:33:46 -0600

    Discovering the wonders of Clerk's UserProfile

---
+++ app/(dashboard)/profile/[[...rest]]/page.js
@@ -0,0 +1,7 @@
+import { UserProfile } from "@clerk/nextjs"
+
+function ProfilePage() {
+  return <UserProfile />
+}
+
+export default ProfilePage
--- app/(dashboard)/profile/page.js
+++
@@ -1,9 +0,0 @@
-function ProfilePage() {
-  return (
-    <div>
-      Happy ProfilePage &#128513;
-    </div>
-  )
-}
-
-export default ProfilePage
