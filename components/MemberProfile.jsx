26404ca18b798e402d9abfb5582bc6d3f2db9baa
Amanda Mark <amarkpark@gmail.com>
2024-07-11 17:22:43 -0600

    small cleanup

--- components/MemberProfile.jsx
+++ components/MemberProfile.jsx
@@ -4,7 +4,6 @@ import { UserButton } from "@clerk/nextjs"
 import { auth, currentUser } from "@clerk/nextjs/server"
 
 const MemberProfile = async () => {
-  const { userId } = auth();
   const user = await currentUser();
   const userEmail = user.emailAddresses[0].emailAddress;
 
@@ -12,7 +11,6 @@ const MemberProfile = async () => {
     <div className="my-4 flex items-center gap-2">
       <UserButton afterSignOutUrl="/" />
       <span className="text-sm">
-        {/* MemberProfile@example.com */}
         {userEmail}
       </span>
     </div>
