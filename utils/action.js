b497f6c92acb41c16b83c2eb6f1d6e64cc25d0dc
Amanda Mark <amarkpark@gmail.com>
2024-07-12 17:25:09 -0600

    Lessons 75-78: Have achieved OpenAI connectivity
    - Configured OpenAI client
    - Tested sending a chat completion and receiving a response

--- utils/action.js
+++ utils/action.js
@@ -1,7 +1,20 @@
 "use server";
+import OpenAI from "openai";
+
+const openaiClient = new OpenAI(process.env.OPENAI_API_KEY);
 
 export const generateChatResponse = async (chatMessage) => {
-  console.log("??? generateChatResponse ??? ==>", chatMessage);
+  console.log("??? Message Submitted ??? ==>", chatMessage);
+  const response = await openaiClient.chat.completions.create({
+    messages: [
+      {role: "system", content: "You are a helpful assistant."},
+      {role: "user", content: chatMessage},
+    ],
+    model: "gpt-3.5-turbo",
+    temperature: 0.3,
+  })
+  console.log("Verify Query ==>", response.choices[0].message);
+  console.log("Response ==>", response);
 
-  return "Hello World";
+  return "Completed Query";
 }
