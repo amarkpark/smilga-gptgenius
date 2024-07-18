b80cdf3e1bd6842ee6ee174a73598ff04e44f8ce
Amanda Mark <amarkpark@gmail.com>
2024-07-18 13:22:00 -0600

    Lesson 86: Displaying destination info

--- components/DestinationDetails.jsx
+++ components/DestinationDetails.jsx
@@ -1,9 +1,23 @@
 
 const DestinationDetails = ({tour}) => {
   console.log(tour);
+  const {title, description, stops} = tour;
   return (
-    <div>
-      DestinationDetails &#127919;
+    <div className="max-w-2xl">
+      <h1 className="text-4xl font-semibold mb-4">{title}</h1>
+      <h1 className="leading-relaxed mb-6">{description}</h1>
+      <ul>
+        {stops.map((stop) => {
+          return (
+            <li
+              key={stop}
+              className="mb-4 bg-base-100 p-4 rounded-xl"
+            >
+              <p>&#127919; {stop}</p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   )
 }
