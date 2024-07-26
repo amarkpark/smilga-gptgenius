e48421ddad8ab304cf984947eb5fd38535682348
Amanda Mark <amarkpark@gmail.com>
2024-07-26 13:17:34 -0600

    Lesson 97: Fetch photo with axios and Unsplash API

--- app/(dashboard)/tours/[id]/page.js
+++ app/(dashboard)/tours/[id]/page.js
@@ -3,6 +3,9 @@ import { redirect } from "next/navigation";
 import { generateTourImage, getOneTour } from "../../../../utils/action"
 import Link from "next/link";
 import Image from "next/image";
+import axios from "axios";
+
+const urlUnsplash = `https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_API_KEY}&query=`;
 
 const DestinationPage = async ({ params }) => {
   const tour = await getOneTour(params.id);
@@ -11,11 +14,36 @@ const DestinationPage = async ({ params }) => {
     redirect("/tours");
   }
 
-  const tourImage = await generateTourImage({
-    city: tour.city,
-    country: tour.country,
+  const {data} = await axios.get(`${urlUnsplash}${tour.city},${tour.country}`)
+  .catch(function (error) {
+    // care of https://axios-http.com/docs/handling_errors
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.error("Error fetching unsplash Data", error.response.data);
+      console.error("Error fetching unsplash Status", error.response.status);
+      console.error("Error fetching unsplash Headers", error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.error("Error fetching unsplash no res", error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.error("Error fetching unsplash setup", error.message);
+    }
+    console.error("Error fetching unsplash config", error.config);
   });
 
+  const tourImage = data?.results[0]?.urls?.raw || null;
+
+  // // This is for generating an image with OpenAPI == $expensive
+  // const tourImage = await generateTourImage({
+  //   city: tour.city,
+  //   country: tour.country,
+  // });
+
+  // @ToDo: Implement a partial loading solution
   return (
     <div>
       <Link href="/tours" className="btn btn-accent mb-12">
